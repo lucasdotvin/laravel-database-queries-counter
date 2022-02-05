cfba82f7fe1aeecc62d5358188ce6a5a54d9acbf
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-05 13:35:21 -0300

    Initial setup

---
+++ src/DBQueriesCounterServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LucasDotDev\DBQueriesCounter;
+
+use Illuminate\Support\ServiceProvider;
+
+class DBQueriesCounterServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+}
