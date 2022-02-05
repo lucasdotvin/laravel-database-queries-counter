86bff4308e67d3e6fbbc4eaa19f37a5d9bd1df2d
lucasdotdev <lucasdotdev@users.noreply.github.com>
2022-02-05 18:15:06 +0000

    Fix styling

--- src/DBQueriesCounterServiceProvider.php
+++ src/DBQueriesCounterServiceProvider.php
@@ -9,7 +9,7 @@ class DBQueriesCounterServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/Traits/' => base_path('tests/Traits')
+            __DIR__.'/Traits/' => base_path('tests/Traits'),
         ], 'laravel-database-queries-counter-traits');
     }
 }
