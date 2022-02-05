45ae3f2a699aa1e30b2914839a76ba44c718a8e7
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-05 14:03:12 -0300

    Create CountsQueries trait

---
+++ src/Traits/CountsQueries.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace LucasDotDev\DBQueriesCounter\Traits;
+
+use Illuminate\Support\Facades\DB;
+
+trait CountsQueries
+{
+    protected bool $hasDispatchedQueryCounter = false;
+
+    protected bool $isCountingQueries = false;
+
+    protected int $queryCount = 0;
+
+    public function whileCountingQueries(callable $callback)
+    {
+        $this->startCountingQueries();
+
+        $result = $callback();
+
+        $this->stopCountingQueries();
+
+        return $result;
+    }
+
+    public function startCountingQueries()
+    {
+        if (!$this->hasDispatchedQueryCounter) {
+            $this->dispatchQueryCounter();
+        }
+
+        $this->isCountingQueries         = true;
+        $this->hasDispatchedQueryCounter = true;
+    }
+
+    public function stopCountingQueries(): int
+    {
+        $this->isCountingQueries = false;
+
+        return $this->getQueryCount();
+    }
+
+    public function getQueryCount(): int
+    {
+        return $this->queryCount;
+    }
+
+    public function assertDatabaseQueriesCount(int $expectedCount)
+    {
+        return $this->assertEquals($expectedCount, $this->getQueryCount());
+    }
+
+    private function dispatchQueryCounter()
+    {
+        DB::listen(function () {
+            if ($this->isCountingQueries) {
+                $this->queryCount++;
+            }
+        });
+    }
+}
