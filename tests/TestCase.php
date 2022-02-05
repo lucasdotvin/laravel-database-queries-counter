52d472dde88b383dc21a59812369a72f06c9ee55
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-05 14:02:59 -0300

    Add CountsQueries trait to TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,12 @@ namespace LucasDotDev\DBQueriesCounter\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
 use LucasDotDev\DBQueriesCounter\DBQueriesCounterServiceProvider;
+use LucasDotDev\DBQueriesCounter\Traits\CountsQueries;
 
 class TestCase extends Orchestra
 {
+    use CountsQueries;
+
     protected function getPackageProviders($app)
     {
         return [
