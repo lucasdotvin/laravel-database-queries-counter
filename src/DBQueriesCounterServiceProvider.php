cac6d8dd9680206101b28ada73cc4e24afd46056
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-02-05 15:13:33 -0300

    Register traits publishing

--- src/DBQueriesCounterServiceProvider.php
+++ src/DBQueriesCounterServiceProvider.php
@@ -8,6 +8,8 @@ class DBQueriesCounterServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/Traits/' => base_path('tests/Traits')
+        ], 'laravel-database-queries-counter-traits');
     }
 }
